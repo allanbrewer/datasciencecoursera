4fa32120a75a903ec4e38154fd601648608f36bf
allanbrewer <allanbrewer@gmail.com>
2015-02-26 19:04:28 -0430

    Rank All

--- R_Programming/Assignment_3/rankall.R
+++ R_Programming/Assignment_3/rankall.R
@@ -1,15 +1,12 @@
-rankhospital <- function(state, outcome, num = "best") {
+rankall <- function(outcome, num = "best") {
+	
 	## Read Outcome file data
 	data <- read.csv("data/outcome-of-care-measures.csv", colClasses = "character")
 
-	## Check if sate is valid
+	## Create State List
 	stateList <- data[,7]
-	stateTrue <- data[(toupper(state)==stateList),]
-	if (nrow(stateTrue) == 0){
-		stop("invalid state")
-	}
-	## Print number of rows with the state name
-	## print(nrow(stateTrue))
+	factorList <- as.factor(data[,7])
+	states <- levels(factorList)
 
 	## Check if outcome is valid
 	## If TRUE assign column nomber
@@ -22,50 +19,67 @@ rankhospital <- function(state, outcome, num = "best") {
 	} else {
 		stop("invalid outcome")
 	}
-	## Check if outcome column is correct
-	## print(outcome)
 
-	## Coerce column to be numeric
-	stateTrue[,outcome] <- as.numeric(stateTrue[,outcome])
+	dataFrame <- data.frame( hospital = character(), state = character(), stringsAsFactors = FALSE)
 
-	## List with 30-day mortality rate values
-	outcomeValue <- stateTrue[,outcome]
 
-	## Remove NA
-	noNA <- !is.na(outcomeValue)
+	## Start loop for each state
+	for(i in 1:length(states)) {
 
-	stateTrue <- stateTrue[noNA,]
+		state <- states[i]
 
-	outcomeValue <- outcomeValue[noNA]
+		stateTrue <- data[(state==stateList),]
 
-	## Ranking
-	x <- as.numeric(nrow(stateTrue))
-	if(tolower(num) == "best") {
-		num <- 1
-	} else if(tolower(num) == "worst") {
-		num <- x
-	} 
+		## Coerce column to be numeric
+		stateTrue[,outcome] <- as.numeric(stateTrue[,outcome])
 
-	if(x < num) {
-		num <- NA
-        print(num)
-	} else {
+		## List with 30-day mortality rate values
+		outcomeValue <- stateTrue[,outcome]
 
-	## Print num value
-	## print(num)
+		## Remove NA
+		noNA <- !is.na(outcomeValue)
 
-	## Get Hospital List
-	hospitals <- stateTrue[,2]
+		stateTrue <- stateTrue[noNA,]
 
-	## Order Hospital List
-	ii <- order(outcomeValue, hospitals)
-	ranking <- rbind(outcomeValue,hospitals)[,ii]
+		outcomeValue <- outcomeValue[noNA]
 
-	## Get Hospital Name
-	hospitalName <- ranking[[2,num]]
+		## Ranking
+		x <- as.numeric(nrow(stateTrue))
+		if(tolower(num) == "best") {
+			y <- 1
+		} else if(tolower(num) == "worst") {
+			y <- x
+		} else {
+			y <- num
+		}
 
-	## Print Hospital Name
-	print(hospitalName)
+		if(x < y) {
+			hospitalName <- NA
+		} else if(x == 1){
+			## Get Hospital List
+			hospitals <- stateTrue[,2]
+			
+			hospitalName <- hospitals
+		} else {
 
-}
+			## Get Hospital List
+			hospitals <- stateTrue[,2]
+
+			## Order Hospital List
+			ii <- order(outcomeValue, hospitals)
+			ranking <- rbind(outcomeValue,hospitals)[,ii]
+
+			## Get Hospital Name
+			hospitalName <- ranking[[2,y]]
+
+		}
+                
+		dataFrame[i,1] <- hospitalName
+		dataFrame[i,2] <- state
+
+	}
+
+	## Print Data Frame
+	print(dataFrame)
+
 }
